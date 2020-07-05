b9c1757f8273c620d599547b93ed56dc530dadb5
David Slusser <dslusser@box.com>
2020-07-05 15:03:04 -0700

    added faq page

--- nexthost/urls/api_v1.py
+++ nexthost/urls/api_v1.py
@@ -6,16 +6,16 @@ from rest_framework.routers import DefaultRouter
 import hostmgr.views.views_api as api
 
 
-app_name = "api"
+app_name = 'api'
 
 router = DefaultRouter()
 
 # hostmgr API Endpoints
-router.register(r'owner', api.OwnerViewSet, "owner")
-router.register(r'project', api.ProjectViewSet, "project")
-router.register(r'pattern', api.PatternViewSet, "pattern")
-router.register(r'assetidtype', api.AssetIdTypeViewSet, "assetidtype")
-router.register(r'hostname', api.HostnameViewSet, "hostname")
+router.register(r'owner', api.OwnerViewSet, 'owner')
+router.register(r'project', api.ProjectViewSet, 'project')
+router.register(r'pattern', api.PatternViewSet, 'pattern')
+router.register(r'assetidtype', api.AssetIdTypeViewSet, 'assetidtype')
+router.register(r'hostname', api.HostnameViewSet, 'hostname')
 
 
 urlpatterns = [
